11a2dbe8a377077af30ffb95f2ef863934ea4e4e
Rasquinha_ <116691516+Bruno-rasq@users.noreply.github.com>
2024-08-11 01:35:46 +0000

    wip:  refatorando os testes

--- src/routers/users.routes.ts
+++ src/routers/users.routes.ts
@@ -14,7 +14,20 @@ router.get("/:id", setDataSource(ProdutionDataSource), controllers.get_user_by_i
 router.put("/:id", setDataSource(ProdutionDataSource), controllers.update_user)
 router.delete("/:id", setDataSource(ProdutionDataSource), controllers.delete_user)
 
-//isso claramente é uma gambiarra pesada. perdâo se ofendi vcs
+
+/*
+
+	ALERTA!  -- ALTOS NIVEIS DE GAMBIARRa
+
+	obs: prometo remover isso imediatamente assim que descobrir
+	uma foram melhor de fazer os testes.
+	
+*/
 router.get("/test", setDataSource(TestDataSource), controllers.get_all_users)
+router.post("/test", setDataSource(TestDataSource), controllers.create_user)
+router.get("/test/:id", setDataSource(TestDataSource), controllers.get_user_by_id)
+router.put("/test/:id", setDataSource(TestDataSource), controllers.update_user)
+router.delete("/test/:id", setDataSource(TestDataSource), controllers.delete_user)
+
 
 export { router };
